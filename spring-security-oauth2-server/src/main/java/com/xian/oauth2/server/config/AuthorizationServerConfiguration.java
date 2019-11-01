4e406df8c29ed364208ec51f0fb12407be10170c
ruanjiayu <1963241849@qq.com>
2019-11-01 10:36:55 +0800

    修改了springboot的版本，将其变成了2.1.4Release版本以及spring-cloud版本。另外实现了通过内存储存获取access-token

--- spring-security-oauth2-server/src/main/java/com/xian/oauth2/server/config/AuthorizationServerConfiguration.java
+++ spring-security-oauth2-server/src/main/java/com/xian/oauth2/server/config/AuthorizationServerConfiguration.java
@@ -1,6 +1,8 @@
 package com.xian.oauth2.server.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
@@ -16,6 +18,10 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.E
 @EnableAuthorizationServer //说明我开启认证服务的意思
 public class AuthorizationServerConfiguration extends AuthorizationServerConfigurerAdapter {
 
+
+    @Autowired
+    private BCryptPasswordEncoder passwordEncoder;
+
     @Override
     public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
         //  配置客户端
@@ -25,12 +31,12 @@ public class AuthorizationServerConfiguration extends AuthorizationServerConfigu
                 // client_id
                 .withClient("client")
                 // client_secret
-                .secret("secret")
+                .secret(passwordEncoder.encode("secret"))
                 // 授权类型
                 .authorizedGrantTypes("authorization_code")
                 // 授权范围
                 .scopes("app")
                 // 注册回调地址
-                .redirectUris("https://www.baidu.com");
+                .redirectUris("http://www.baidu.com");
     }
 }
--- spring-security-oauth2-server/src/main/java/com/xian/oauth2/server/config/WebSecurityConfiguration.java
+++ spring-security-oauth2-server/src/main/java/com/xian/oauth2/server/config/WebSecurityConfiguration.java
@@ -1,10 +1,12 @@
 package com.xian.oauth2.server.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 /**
  * @Description:
@@ -19,4 +21,18 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
 
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("admin").password(passwordEncoder().encode("123456")).roles("ADMIN")
+                .and()
+                .withUser("user").password(passwordEncoder().encode("123456")).roles("USER");
+
+    }
 }
